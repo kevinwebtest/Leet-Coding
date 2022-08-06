77fd7500623151cf9b13daf7bc69a0a0e0b87be4
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-08-07 00:21:41 +0700

    Create Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if ((i=="(") or (i=="[") or (i=="{")):
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                lastappendstack = stack.pop()
+                if (((i == ")") & (lastappendstack !="(")) | ((i == "]") & (lastappendstack !="[")) | ((i == "}") & (lastappendstack !="{"))):
+                    return False
+        return len(stack) == 0
