7a9c950c47f7e206ecb93ed163a698b4e4065507
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-08-13 14:34:15 +0700

    Update Binary Tree Traversal.py

--- Binary Tree Traversal.py
+++ Binary Tree Traversal.py
@@ -12,21 +12,21 @@ class Solution:
         self.inorder(root, output3)
         return output1,output2,output3
         
-    def inorder(self, myroot, output):
+    def inorder(self, myroot, output1):
         if myroot:
-            self.inorder(myroot.left, output)
-            output.append(myroot.val)
-            self.inorder(myroot.right, output)
+            self.inorder(myroot.left, output1)
+            output1.append(myroot.val)
+            self.inorder(myroot.right, output1)
             
-    def preorder(self, myroot, output):
+    def preorder(self, myroot, output2):
         if myroot:
-            output.append(myroot.val)
-            self.preorder(myroot.left, output)
-            self.preorder(myroot.right, output)
+            output2.append(myroot.val)
+            self.preorder(myroot.left, output2)
+            self.preorder(myroot.right, output2)
             
-    def inorder(self, myroot, output):
+    def inorder(self, myroot, output3):
         if myroot:
-            self.inorder(myroot.left, output)
-            self.inorder(myroot.right, output)
-            output.append(myroot.val)
+            self.inorder(myroot.left, output3)
+            self.inorder(myroot.right, output3)
+            output3.append(myroot.val)
     
