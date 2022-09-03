0225513c4e5460473445b994f981f7a2389480d8
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-09-03 22:19:47 +0700

    Update Nth Highest Salary.sql

--- SQL/Nth Highest Salary.sql
+++ SQL/Nth Highest Salary.sql
@@ -8,3 +8,13 @@ SET N=N-1;
       LIMIT 1 OFFSET N
   );
 END
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      SELECT e1.Salary
+      FROM (SELECT DISTINCT Salary FROM Employee) e1
+      WHERE (SELECT COUNT(*) FROM (SELECT DISTINCT Salary FROM Employee) e2 WHERE e2.Salary > e1.Salary) = N - 1      
+      LIMIT 1	
+  );
+END
