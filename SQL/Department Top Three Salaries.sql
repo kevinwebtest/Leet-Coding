e8c82c615c6d9ac8f6848db37c9186c590e68bac
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-09-03 21:22:41 +0700

    Update Department Top Three Salaries.sql

--- SQL/Department Top Three Salaries.sql
+++ SQL/Department Top Three Salaries.sql
@@ -4,3 +4,12 @@ Left join Employee as tE2 on tE1.DepartmentId = tE2.DepartmentId and tE1.Salary 
 group by tE1.Id
 having count(distinct tE2.Salary) <= 3
 order by tE1.DepartmentId, tE1.Salary desc
+
+
+SELECT d.name AS Department, e.name AS Employee, salary AS Salary 
+FROM Employee e, Department d
+WHERE e.departmentId = d.id
+AND 3 > (SELECT COUNT(DISTINCT e2.salary)
+        FROM Employee e2
+        WHERE e2.departmentId = e.departmentId
+        AND e2.Salary>e.Salary)
