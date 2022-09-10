5f9c9b338f03fb2542a752a4426d53adb7d06153
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-09-10 22:57:47 +0700

    Create Best Time to Buy and Sell Stock IV(Hard).py

---
+++ Best Time to Buy and Sell Stock IV(Hard).py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        
+        if k >= len(prices) // 2:
+            output = 0
+            for i in range(len(prices)-1):
+                output += max(prices[i+1]-prices[i],0)
+            return output
+        
+        profits = [0]*len(prices)
+        for j in range(k):
+
+            preprofit = 0
+            for i in range(1,len(prices)):
+            
+                profit = prices[i] - prices[i-1]
+                preprofit = max(preprofit+profit, profits[i])
+                profits[i] = max(profits[i-1], preprofit)
+    
+        return profits[-1]
