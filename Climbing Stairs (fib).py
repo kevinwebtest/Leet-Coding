b8bfdd761a4276fd612406478c31cfcde3152f52
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-09-26 21:03:13 +0700

    Update Climbing Stairs (fib).py

--- Climbing Stairs (fib).py
+++ Climbing Stairs (fib).py
@@ -6,3 +6,13 @@ class Solution:
             one = one + two
             two = temp
         return one
+    
+    def fibbonaci(n):
+        if n==1 or n==2:
+            return 1
+        fibList = [None for i in range(n+1)]
+        fibList[1] = 1
+        fibList[2] = 1
+        for i in range(3,n+1):
+            fibList[i] = fibList[i-1] + fibList[i-2]
+        return fibList[n]
