b314d8cd83053017d92bbacb55699b3c09fc6638
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-09-27 22:25:08 +0700

    Create Can target be summed from list.py

---
+++ Dynamic Programming/Can target be summed from list.py
@@ -0,0 +1,35 @@
+# Check if the list numbers can added up to the target (Every number can be used more than once)
+def canSum(target, numbers, memo = {}):
+  if target in memo:
+    return memo[target]
+  if target==0:
+    return True
+  if target<0:
+    return False
+
+  for num in numbers:
+    remainder = target - num
+    if canSum(remainder, numbers, memo)==True:
+      memo[target] = True
+      return True
+  memo[target] = False
+  return False
+
+# Second code
+def canSum(target, numbers, memo = {}):
+  if target in memo:
+    return memo[target]
+  if target==0:
+    return True
+  if target<0:
+    return False
+
+  for num in numbers:
+    remainder = target - num
+    memo[remainder] = canSum(remainder, numbers, memo)
+    if memo[remainder]==True:
+      return True
+  return False
+
+memo ={}
+print(canSum(300,[14,7,15],memo),memo)
