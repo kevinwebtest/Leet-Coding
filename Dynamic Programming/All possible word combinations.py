745887fd5e3998cfa316e6611ab9804899d7955e
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-09-30 01:21:58 +0700

    Create All possible word combinations.py

---
+++ Dynamic Programming/All possible word combinations.py
@@ -0,0 +1,22 @@
+def allWordComb(target, words,memo={}):
+  if target in memo:
+    return memo[target]
+  if target == "":
+    return [[]]
+  output = []
+  for word in words:
+    if target.startswith(word) == True:
+      sliced = target[len(word):]
+      wordCombs = allWordComb(sliced,words,memo)
+      if len(x)>0:
+        for wordComb in wordCombs:
+          wordComb.insert(0,word)
+      output += wordCombs
+  memo[target]=output
+  return output
+
+print(allWordComb("purple", ["purp", "p", "ur", "le", "purpl"]))
+print(allWordComb("abcdef", ["ab", "abc", "cd", "def", "abcd","ef","c"]))
+print(allWordComb("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"],{}))
+print(allWordComb("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"],{}))
+print(allWordComb("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee", "eeeeeeeee"],{}))
