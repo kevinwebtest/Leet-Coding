7f39f3f1e2d5df66a6404fd36b6a3e10fe45f153
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-09-29 18:21:08 +0700

    Create Can target be constructed by words.py

---
+++ Dynamic Programming/Can target be constructed by words.py
@@ -0,0 +1,35 @@
+# Can the list words be constructed to make the word 'target'
+def canWordComb(target, words, memo={}):
+  if target in memo:
+    return memo[target]
+  if target == "":
+    return True
+  for word in words:
+    if target.startswith(word) == True:
+      sliced = target[len(word):]
+      canWC = canWordComb(sliced, words, memo)
+      memo[sliced] = canWC
+      if canWC == True:
+        return True
+  return False
+
+# or
+def canWordComb(target, words, memo={}):
+  if target in memo:
+    return memo[target]
+  if target == "":
+    return True
+  for word in words:
+    if target.startswith(word) == True:
+      sliced = target[len(word):]
+      canWC = canWordComb(sliced, words, memo)
+      if canWC == True:
+        memo[sliced] = True
+        return True
+  memo[sliced] = False
+  return False
+
+
+print(canWordComb("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(canWordComb("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(canWordComb("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee", "eeeeeeeee"]))
