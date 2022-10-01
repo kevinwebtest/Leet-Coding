e04493dd4850501b28a5808c4135738cab425881
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-01 15:49:02 +0700

    Update Fibonacci.py

--- Dynamic Programming/Fibonacci.py
+++ Dynamic Programming/Fibonacci.py
@@ -1,12 +1,4 @@
-def fib2(n):
-  fibo = [0 for i in range(n+3)]
-  fibo[1] = 1
-  for num in range(n+1):
-    fibo[num+1] += fibo[num]
-    fibo[num+2] += fibo[num]
-  return fibo[n]
-print(fib2(50))
-
+# Recursive version
 def fib(n, memo={}):
   if n in memo:
     return memo[n]
@@ -16,5 +8,15 @@ def fib(n, memo={}):
     return 1
   memo[n] = fib(n-1)+fib(n-2)
   return memo[n]
+print(fib(50)) #12586269025def 
 
-print(fib(50)) #12586269025
+# Tabular version
+fib2(n):
+  fibo = [0 for i in range(n+3)]
+  fibo[1] = 1
+  for num in range(n+1):
+    fibo[num+1] += fibo[num]
+    fibo[num+2] += fibo[num]
+  return fibo[n]
+print(fib2(50))
+
