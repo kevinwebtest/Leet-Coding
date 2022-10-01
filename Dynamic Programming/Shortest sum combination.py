d6cd0844985b2d7d09a5967a935600e16cf1b705
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-02 00:09:24 +0700

    Update Shortest sum combination.py

--- Dynamic Programming/Shortest sum combination.py
+++ Dynamic Programming/Shortest sum combination.py
@@ -11,11 +11,11 @@ def shortestSumComb(target, numbers, memo={}):
   for num in numbers:
     remainder = target - num
     currentComb = shortestSumComb(remainder, numbers, memo)
-    memo[remainder] = currentComb
     if currentComb != None:
       currentComb.append(num)
       if (shortestComb==None or len(currentComb)<len(shortestComb)):
         shortestComb = currentComb.copy()
+  memo[target] = shortestComb
   return shortestComb
     
 memo={}
@@ -26,3 +26,22 @@ memo={}
 print(shortestSumComb(8,[1,4,5],memo),memo) #[4, 4]
 print(shortestSumComb(7,[4,2],{})) #None
 print(shortestSumComb(300,[14,7,15],{})) #None
+
+
+def bestSumComb(target, numbers):
+  combination = [None for i in range(target+1)]
+  combination[0] = []
+  for x in range(target+1):
+    if combination[x] != None:
+      for num in numbers:
+        if x+num<=target:
+          if (combination[x+num]!= None) and len(combination[x] + [num]) > len(combination[x+num]):
+            continue
+          combination[x+num] = combination[x] + [num]
+  return combination[target]
+
+print(bestSumComb(7,[2,3])) #[3, 2, 2]
+print(bestSumComb(7,[5,3,4,7])) #[7]
+print(bestSumComb(7,[4,2])) #None
+print(bestSumComb(8,[2,3,5])) #[3,5]
+print(bestSumComb(300,[14,7])) #None
