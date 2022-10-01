8559b6b2a7c4287a0bbf4cd0d3a2801b0d196ebe
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-02 01:27:16 +0700

    Update Can target be constructed by words.py

--- Dynamic Programming/Can target be constructed by words.py
+++ Dynamic Programming/Can target be constructed by words.py
@@ -1,4 +1,5 @@
 # Can the list words be constructed to make the word 'target'
+# Recursive version
 def canWordComb(target, words, memo={}):
   if target in memo:
     return memo[target]
@@ -29,7 +30,21 @@ def canWordComb(target, words, memo={}):
   memo[target] = False
   return False
 
-
 print(canWordComb("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
 print(canWordComb("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
 print(canWordComb("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee", "eeeeeeeee"]))
+
+# Tabular version
+def canConstruct(target, words):
+  boolTarget = [False for i in range(len(target)+1)]
+  boolTarget[0] = True
+  for x in range(len(boolTarget)):
+    if boolTarget[x] == True:
+      for word in words:
+        if target[x:x+len(word)] == word:
+          boolTarget[x+len(word)] = True
+  return boolTarget[len(target)]
+
+print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee", "eeeeeeeee"]))
