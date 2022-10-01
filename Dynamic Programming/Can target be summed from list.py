c3d8da19368e31487c34b0144c68a19ba10138bb
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-01 16:18:35 +0700

    Update Can target be summed from list.py

--- Dynamic Programming/Can target be summed from list.py
+++ Dynamic Programming/Can target be summed from list.py
@@ -32,9 +32,24 @@ def canSum(target, numbers, memo = {}):
       return True
   return False
 
-# Tabular version
 print(canSum(7,[2,3],{}) #True
 print(canSum(7,[4,2],{}) #False
 # Hati-hati karena python ingat memo/dictionary yang di dalam function ketika pemanggilan selanjutnya jika memo dikosongkan saat pemanggilan function
 memo ={}
 print(canSum(300,[14,7],memo),memo) #False
+      
+# Tabular version
+def canSum(target, numbers):
+  targetBool = [False for i in range(target+1)]
+  targetBool[0] = True
+  for x in range(target+1):
+    if targetBool[x] == True:
+      for num in numbers:
+        if x+num<=target:
+          targetBool[x+num] = True
+  return targetBool[target]
+print(canSum(7,[3,6,5])) #False
+print(canSum(7,[5,3,4,7])) #True
+print(canSum(7,[2,3])) #True
+print(canSum(13,[4,2])) #False
+print(canSum(300,[14,7])) #False
