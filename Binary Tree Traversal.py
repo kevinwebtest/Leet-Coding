e74cca402a8afe19dca3d396521d6dd6ccf1c8ee
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-03 18:19:17 +0700

    Update Binary Tree Traversal.py

--- Binary Tree Traversal.py
+++ Binary Tree Traversal.py
@@ -30,3 +30,14 @@ class Solution:
             self.postorder(myroot.right, output3)
             output3.append(myroot.val)
     
+class Solution:
+    def preorderTraversal(self, root):
+        ret = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return ret
