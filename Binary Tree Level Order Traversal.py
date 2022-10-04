c816b05b620f33ff3ca31ed3b451327b6f1eebb6
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-04 11:19:15 +0700

    Update Binary Tree Level Order Traversal.py

--- Binary Tree Level Order Traversal.py
+++ Binary Tree Level Order Traversal.py
@@ -9,8 +9,19 @@ class Solution:
         orderLevel(root, 0, output)
         newoutput=[]
         if output:
-            length,_ = max(output)
+            length = max(output)[0]
             newoutput= [[] for it in range(length+1)]
             for i, num in output:
                 newoutput[i].append(num)
         return newoutput
+    
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root==None:
+            return []
+        output, level = [], [root]
+        while level:
+            output.append([node.val for node in level])
+            temp = [[node.left,node.right] for node in level]
+            level = [notnull for leftright in temp for notnull in leftright if notnull]
+        return output
