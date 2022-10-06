38806787f044f728d7afc3fff071f874ba6d16ae
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-06 22:56:04 +0700

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -4,3 +4,12 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j] == target:
                     return i,j
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        memo = {}
+        for i in range(len(nums)):
+            remainder = target-nums[i]
+            if remainder in memo:
+                return [memo[remainder],i]
+            else:
+                memo[nums[i]] = i
