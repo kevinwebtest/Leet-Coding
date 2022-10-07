014a0410a29703e676711cd5c1e7c21dd514acee
Kevin Setiawan <kevinsetiawan005@gmail.com>
2022-10-07 17:45:44 +0700

    Create Tree Decrements.py

---
+++ TikTok/Tree Decrements.py
@@ -0,0 +1,29 @@
+import math
+def getMinCost(val, t_nodes, t_from, t_to):
+    for i in range(len(val)):
+        val[i] %= 2
+
+    adj = [[] for _ in range(t_nodes)]
+    for i in range(len(t_from)):
+        u, v = t_from[i] - 1, t_to[i] - 1
+        adj[u].append(v)
+        adj[v].append(u)
+
+    def merge(dp1, dp2):
+        res = [math.inf for _ in range(2)]
+        res[0] = min(dp1[0] + dp2[0], dp1[1] + dp2[1] + 1)
+        res[1] = min(dp1[0] + dp2[1] + 1, dp1[1] + dp2[0])
+        return res
+
+    dp = [[math.inf for _ in range(2)] for _ in range(t_nodes)]
+    def dfs(u, p):
+        dp[u][val[u]] = 0
+
+        for i in adj[u]:
+            if i == p:
+                continue
+            dfs(i, u)
+            dp[u] = merge(dp[u], dp[i])
+
+    dfs(0, -1)
+    return dp[0][0]
