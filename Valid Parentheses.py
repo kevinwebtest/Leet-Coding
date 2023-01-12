dc83d60cd3c69bfaff547f949e8a149b69e73bbf
Kevin Setiawan <kevinsetiawan005@gmail.com>
2023-01-12 17:17:44 +0700

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -13,3 +13,18 @@ class Solution:
                     | ((i == "}") & (lastappendstack !="{"))):
                     return False
         return len(stack) == 0
+    
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
